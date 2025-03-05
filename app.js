d88e593902775c8a867546275726e7ccf6fad277
Michael <mpborer@gmail.com>
2025-03-05 13:57:41 -0700

    javaeasy?

---
+++ app.js
@@ -0,0 +1,13 @@
+// function booleanInteger(num){
+//    if (num % 2 === 0){
+//        return (1)
+//     }
+//     return (-1)
+// }
+// console.log(booleanInteger(1))
+
+// one line method
+function booleanInteger(num){
+    return num % 2 === 0 ? 1 : -1
+}
+console.log(booleanInteger(1))
